3b80f960e7e555bcdaad6a8db8d4e004c8ceebd7
DeeDee Lavinder <deedeelavinder@gmail.com>
2014-11-09 01:37:28 -0500

    corrected routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'legislators/index'
 
-  resources :locations
+  resources :locations, only: [:new, :create]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
