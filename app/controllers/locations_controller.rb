40c0a0942e0307e6ffbc8518198cc4471d1d4d6c
DeeDee Lavinder <deedeelavinder@gmail.com>
2014-11-09 20:09:39 -0500

    error messages working for all cases, whoop whoop

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -8,15 +8,18 @@ class LocationsController < ApplicationController
 
   def search
     location = Geocoder.search(location_params[:address]).first
-    unless location.nil?
-      @sunlight_response = HTTParty.get('https://congress.api.sunlightfoundation.com/legislators/locate',
-                                        query: {latitude: location.latitude, longitude: location.longitude},
-                                        headers: {"X-APIKEY" => "c31f9f2742674a9f8ee4e48183d8378e"})
-      @legislators = @sunlight_response['results']
-      render :'legislators/index'
-    else
+    if location.nil?
       @error = "Please enter a valid address."
       render 'new'
+    elsif location = []
+      @error = "Please enter a valid address."
+      render 'new'
+    else
+      @sunlight_response = HTTParty.get('https://congress.api.sunlightfoundation.com/legislators/locate',
+                              query: {latitude: location.latitude, longitude: location.longitude},
+                                      headers: {"X-APIKEY" => "c31f9f2742674a9f8ee4e48183d8378e"})
+      @legislators = @sunlight_response['results']
+      render :'legislators/index'
     end
   end
 
