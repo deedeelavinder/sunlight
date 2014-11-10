a708b4ed72b02f72062516a54afd960c84c62cfc
Nick Passarella <nickpassarella@gmail.com>
2014-11-10 01:33:23 -0500

    cleaned up unneeded files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,50 +14,6 @@
 //= require jquery_ujs
 //= require_tree .
 
-
-
-//$.ajax({
-//    type: "GET",
-//    dataType: "json",
-//    url: "https://congress.api.sunlightfoundation.com/legislators/locate?apikey=c31f9f2742674a9f8ee4e48183d8378e",
-//    data: "zip=27701",
-//    success: function (json) {
-//
-//
-//        var leg = json.results[1];
-//        if (leg.title === "Rep") {
-//            office = "House Representative"
-//        } else if (leg.title === "Sen" && leg.state_rank === "senior") {
-//            office = "Senior Senator"
-//        } else if (leg.title === "Sen" && leg.state_rank === "junior") {
-//            office = "Junior Senator"
-//        } else {
-//            office = "Delegate"
-//        }
-//
-//        if (leg.party === "R") {
-//            party = "Republican"
-//        } else if (leg.party === "D") {
-//            party = "Democrat"
-//        } else {
-//            party = "Independent"
-//        }
-//
-//        $("h2").html(leg.first_name + " " + leg.last_name);
-//        $(".photo").html('<img src="http://theunitedstates.io/images/congress/225x275/' + leg.bioguide_id + '.jpg"></img>');
-//        $("img").error(function () {
-//            $(this).unbind("error").attr("src", "http://www.keralabjp.org/images/No_Image.jpg");
-//        });
-//        $(".twitter_handle").html("<a href='https://twitter.com/intent/user?screen_name=" + leg.twitter_id + "'><i class='fa fa-twitter-square'></i> @" + leg.twitter_id + "</a>");
-//        $(".header").html(office);
-//        $(".start_date").html(leg.term_start);
-//        $(".end_term_date").html(leg.term_end);
-//        $(".party").html(party);
-//        $(".phone_number").html('<a href="tel:+' + leg.phone + '"><i class="fa fa-phone-square"></i> ' + leg.phone + '</a>');
-//        $(".message").attr("href", leg.contact_form);
-//
-//    }});
-
 $(document).on('ready', function () {
     $('img').error(function () {
         $(this).attr('src', 'assets/image_404.jpg');
