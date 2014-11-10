07185483e54c6030cfc252915f730155d22d9013
Nick Passarella <nickpassarella@gmail.com>
2014-11-10 01:08:21 -0500

    Lots of things working. IP Address is a go. Styling is almost complete.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,19 +3,19 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def location
-    if params[:location].blank?
-      if Rails.env.test? || Rails.env.development?
-        @location ||= Geocoder.search("50.78.167.161").first
-      else
-        @location ||= request.location
-      end
+  def remote_ip
+    if request.remote_ip == '127.0.0.1'
+      '98.122.165.49'
     else
-      params[:location].each {|l| l = l.to_i } if params[:location].is_a? Array
-      @location ||= Geocoder.search(params[:location]).first
-      @location
+      request.remote_ip
     end
   end
 
-
+  def api_response(latitude, longitude)
+    @sunlight_response = HTTParty.get('https://congress.api.sunlightfoundation.com/legislators/locate',
+                                      query: {latitude: latitude, longitude: longitude},
+                                      headers: {"X-APIKEY" => "c31f9f2742674a9f8ee4e48183d8378e"})
+    @legislators = @sunlight_response['results']
+    render :'legislators/index'
+  end
 end
--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,46 +1,24 @@
-
 class LocationsController < ApplicationController
   include HTTParty
-  before_action :set_location, only: [:show, :edit, :update, :destroy]
-
-  def new
-  end
 
   def search
-    location = Geocoder.search(location_params[:address]).first
-    if location.nil?
-      @error = "Please enter a valid address."
-      render 'new'
-    elsif location_params.empty?
+    if location_params[:address] == "use my IP please!!!"
+      location = Geocoder.coordinates(remote_ip)
+      latitude = location[0]
+      longitude = location[1]
+      api_response(latitude, longitude)
+    elsif location_params[:address].empty?
       @error = "Please enter a valid address."
       render 'new'
     else
-      @sunlight_response = HTTParty.get('https://congress.api.sunlightfoundation.com/legislators/locate',
-                                        query: {latitude: location.latitude, longitude: location.longitude},
-                                        headers: {"X-APIKEY" => "c31f9f2742674a9f8ee4e48183d8378e"})
-      @legislators = @sunlight_response['results']
-      render :'legislators/index'
+      location = Geocoder.search(location_params[:address]).first
+      api_response(location.latitude, location.longitude)
     end
   end
-
-  # def search
-  #   location = Geocoder.search(location_params[:address]).first
-  #   unless location.nil?
-  #     @sunlight_response = HTTParty.get('https://congress.api.sunlightfoundation.com/legislators/locate',
-  #                                       query: {latitude: location.latitude, longitude: location.longitude},
-  #                                       headers: {"X-APIKEY" => "c31f9f2742674a9f8ee4e48183d8378e"})
-  #     @legislators = @sunlight_response['results']
-  #     render :'legislators/index'
-  #   else
-  #     @error = "Please enter a valid address."
-  #     render 'new'
-  #   end
-  # end
 
   private
 
   def location_params
     params.permit(:address, :latitude, :longitude)
   end
-
 end
